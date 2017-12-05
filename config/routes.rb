d1454d92edd9a0c04968a84dd3d9c665cfbe677d
tono77 <edgardo.ochoa@gmail.com>
2017-12-04 23:04:45 -0300

    Vista nueva en controlador pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages/one'
   root 'pages#one'
   get 'pages/two'
+  get 'pages/three'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
