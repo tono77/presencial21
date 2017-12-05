ae9b0bbeca780467385ad962deb3ae6689aa8309
tono77 <edgardo.ochoa@gmail.com>
2017-12-04 22:58:45 -0300

    Nuevas vistas y controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/one'
+  root 'pages#one'
+  get 'pages/two'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
